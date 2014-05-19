9ad8c12b3ed57024c1c2532739630c958c0b123d
Bryan Belanger <bbelanger@azcender.com>
2014-05-19 16:50:09 -0400

    Added parent directory creation for yum repos

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -1,4 +1,21 @@
 class profiles::yum inherits profiles {
+  # Create parent directory for repo
+  file { '/var':
+    ensure => 'directory'
+  } ->
+
+  file { '/var/yumrepos':
+    ensure => 'directory'
+  } ->
+
+  file { '/var/cache':
+    ensure => 'directory'
+  } ->
+
+  file { '/var/cache/yumrepos':
+    ensure => 'directory'
+  } ->
+
   createrepo { 'yumrepo':
     repository_dir => '/var/yumrepos/yumrepo',
     repo_cache_dir => '/var/cache/yumrepos/yumrepo'

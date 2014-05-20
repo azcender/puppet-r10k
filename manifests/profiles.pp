13e740c3ad856dde4ed0486f1f34ed67af3b4047
Bryan Belanger <bbelanger@azcender.com>
2014-05-19 22:00:53 -0400

    Added Azcender yum repo to base profile

--- manifests/profiles.pp
+++ manifests/profiles.pp
@@ -1,4 +1,11 @@
 class profiles {
   # Create any defined hosts
   # create_resources('host', hiera('hosts'))
+
+  # Set azcender yum repo
+  yumrepo { 'azcender':
+    baseurl => 'http://yum.azcender.com',
+    enabled => 1,
+    gpgcheck => 0,
+  }
 }

b4d268c72bb51cf1d85e436a4b36d5987a477186
Paul Talbot <prolixalias@gmail.com>
2014-06-06 11:02:24 -0400

    Initial commit - working POC

---
+++ README.md
@@ -0,0 +1,67 @@
+puppet-r10k
+=================
+
+Vagrant environment for demoing PE3 w/ [r10k](http://github.com/adrienthebo/r10k) and current best practices for building out
+self-contained environments (classification, code & data bindings) per recent Pro Services
+discussions. 
+
+The r10k setup leverages the work done in the zack/r10k Forge module. This includes a 
+custom pre-run command in puppet.conf and an MCollective plug-in to allow r10k operations 
+via Live Management in the Enterprise Console.
+
+Also, the environments built out by r10k aim to be self-contained. Meaning the directories include:
+  * hierdata directory
+  * modules directory built out via Puppetfile
+  * manifests/site.pp for classification via code
+
+Note that this structure for self-contained environments is roughly compatible with the recent
+work done by Eric Shamow and Carl Caum on Puppet Enterprise Continuous Delivery support.
+
+# References & Supporting Materials
+
+Supporting materials to help you grok the problem r10k tries to solve and the approach:
+
+  * [Git Submodules Are Probably Not The Anwser](http://somethingsinistral.net/blog/git-submodules-are-probably-not-the-answer/)
+  * [Rethinking Puppet Deployment](http://somethingsinistral.net/blog/rethinking-puppet-deployment/)
+  * [Puppet Infrastructure with R10K](http://terrarum.net/administration/puppet-infrastructure-with-r10k.html)
+
+# Requirements
+  * VirtualBox >= 4.2
+  * Vagrant >= 1.2
+  * vagrant-oscar ( '$ vagrant plugin install oscar' )
+  * vagrant-pe_build ( '$ vagrant plugin install vagrant-pe_build' )
+  * vagrant-auto_network ( '$ vagrant plugin install vagrant-auto_network' )
+  * vagrant-hosts ( '$ vagrant plugin install vagrant-hosts' )
+  * librarian-puppet ( '$ sudo gem install librarian-puppet' )
+
+# Upgrades
+The environment has been ported to the Vagrant oscar plugin as of release 2.0.0. Users of 1.0.0 will
+almost certainly want to wipe out old VMs and settings before doing a 'vagrant up' with the new release:
+  
+    $ cd <repo>
+    $ vagrant destroy -f
+    $ rm -rf .vagrant
+    $ (cd puppet && rm -rf modules)
+    $ git pull
+
+# Usage 
+(perhaps after Upgrade directions above)
+
+    $ cd <repo>
+    $ (cd puppet && librarian-puppet install --verbose)
+    $ vagrant up
+
+# Notes
+  * The vagrant environment will download the required Vagrant baseboxes if they've not already been installed. This can result in quite a long first run.
+  * Login to console via: https://<master eth1 IP> w/ creds: admin@puppetlabs.com/puppetlabs.
+  * r10k builds out environments in master:/etc/puppetlabs/puppet/environments based on the branches in githhub.com/nvalentine-puppetlabs/demo-pe3-r10k-environments. You can point r10k to a different repo by modifying the Hiera key in puppet/hierdata/common.yaml.
+
+# Troubleshooting
+  * vagrant-hosts sometimes fails to insert the master's hostname into the /etc/hosts files on the agent VMs. One work-around is to run 'vagrant provision --provision-with hosts' a couple of times until the necessary entries have been generated.
+  * For bug reports, the following command will be surpremely useful (to me):
+
+    $ VAGRANT_LOG=DEBUG vagrant up 2>&1 | tee /tmp/demo-pe-r10k-vagrant.runlog
+
+# Implementation details
+  * There's currently a bug in the vagrant-auto_network plugin that necessitates having each VM
+initially assiged the IP address 0.0.0.0. This is a temporary hack.
---
+++ puppet/manifests/site.pp
@@ -0,0 +1,152 @@
+node 'base' {
+  include ntp
+
+  @@host { $::hostname:
+    ensure         => present,
+    ip             => $::virtual ? {
+      'virtualbox' => $::ipaddress_eth1,
+      default      => $::ipaddress_eth0,
+    },
+    host_aliases   => $hostname,
+  }
+
+  host { 'localhost':
+    ensure       => present,
+    ip           => '127.0.0.1',
+    host_aliases => 'localhost.localdomain',
+  }
+
+  Host <<||>>
+
+  ini_setting { 'set puppet agent environment':
+    ensure   => present,
+    path     => '/etc/puppetlabs/puppet/puppet.conf',
+    section  => 'agent',
+    setting  => 'environment',
+    value    => 'dev',
+  }
+
+##### mDNS - begin #####
+# case $::osfamily {
+#
+#   'redhat': {
+#     exec{'retrieve_epel_key':
+#       command => "/usr/bin/wget --no-check-certificate -q https://fedoraproject.org/static/217521F6.txt -O /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL",
+#       creates => "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL",
+#     }
+#     file{'/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL':
+#       owner   => root,
+#       group   => root,
+#       mode    => 0444,
+#       require => Exec["retrieve_epel_key"],
+#     }
+#     yumrepo { "epel":
+#       mirrorlist => 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=$basearch',
+#       enabled    => 1,
+#       gpgcheck   => 1,
+#       gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL",
+#       require    => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL"]
+#     }
+#     $mdns_packages = ['nss-mdns', 'avahi']
+#     package { 'nss-mdns':
+#       ensure  => installed,
+#       require => Yumrepo[ "epel" ],
+#     }
+#     package { 'avahi':
+#       ensure  => installed,
+#       require => Package['nss-mdns'],
+#     }
+#     service { 'avahi-daemon':
+#       ensure   => running,
+#       require  => Package['nss-mdns'],
+#     }
+#   }
+#
+#   'debian': {
+#     $mdns_packages = ['lib32nss-mdns', 'avahi-daemon']
+#     package { $mdns_packages: ensure => installed }
+#   }
+#
+#   default: {
+#     $mdns_packages = ['nss-mdns', 'avahi']
+#     package { $mdns_packages: ensure => installed }
+#   }
+# }
+##### mDNS - end #####
+
+}
+
+node /^master.*$/ inherits base {
+  if $::osfamily == 'redhat' {
+    class { 'firewall': ensure => stopped, }
+  }
+
+  package { 'git': ensure => present, }
+
+  file { 'r10k environments dir':
+    ensure   => directory,
+    path     => '/etc/puppetlabs/puppet/environments',
+  }
+
+  class { 'r10k':
+    remote   => hiera('r10k_repo', 'git@bitbucket.org:prolixalias/puppet-r10k-environments.git')
+  }
+
+  exec { 'r10k deploy environment --puppetfile':
+    path     => ['/bin','/sbin','/usr/bin','/usr/sbin','/opt/puppet/bin'],
+    require  => [Package['git'],File['r10k environments dir'],Class['r10k::install']],
+  }
+
+# include r10k::prerun_command
+  include r10k::mcollective
+
+  ini_setting { 'master module path':
+    ensure   => present,
+    path     => '/etc/puppetlabs/puppet/puppet.conf',
+    section  => 'main',
+    setting  => 'modulepath',
+    value    => '/etc/puppetlabs/puppet/environments/$environment/modules:/opt/puppet/share/puppet/modules',
+  }
+
+  ini_setting { 'master manifest path':
+    ensure   => present,
+    path     => '/etc/puppetlabs/puppet/puppet.conf',
+    section  => 'main',
+    setting  => 'manifest',
+    value    => '/etc/puppetlabs/puppet/environments/$environment/manifests/site.pp',
+  }
+
+# hiera.yaml is read only at master startup thus this will not work!
+#  ini_setting { 'hiera path':
+#    ensure  => present,
+#    path    => '/etc/puppetlabs/puppet/puppet.conf',
+#    section => 'main',
+#    setting => 'heira_config',
+#    value   => '/etc/puppetlabs/puppet/environments/$environment/hiera.yaml',
+#  }
+
+  file { '/etc/puppetlabs/puppet/hiera.yaml':
+    ensure   => file, mode => '0755', owner => 'root', group => 'root',
+    source   => 'file:///vagrant/puppet/files/hiera.yaml',
+    notify   => Service['pe-httpd'],
+  }
+
+  service { 'pe-httpd': ensure => running, }
+
+  Ini_setting['set puppet agent environment'] {
+    value => 'production',
+  }
+}
+
+node default inherits base {
+  notify { "Node ${::hostname} received default node classification!": }
+  file { '/tmp/runpuppet.sh':
+    ensure => 'file',
+    mode =>  '0755',
+    owner => 'root',
+    group => 'root',
+    content => "#!/bin/bash\npuppet agent -t",
+  } -> exec { 'at now + 1 min -f /tmp/runpuppet.sh':
+    path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', '/opt/puppet/bin'],
+  }
+}

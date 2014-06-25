7944044ee4639e220c3626e7fc96973da81b75c6
Bryan Belanger <bbelanger@azcender.com>
2014-06-25 13:19:47 -0400

    Added disallow stack line to avahi conf

--- manifests/avahi.pp
+++ manifests/avahi.pp
@@ -3,4 +3,11 @@ class profile::avahi {
   class{ '::avahi':
     firewall => 'true'
   }
+
+  # Need to disallow other services
+  file_line {'disallow_other_stacks_in_avahi':
+    path  => '/etc/avahi/avahi-daemon.conf',
+    line  => 'disallow-other-stacks=yes',
+    match => '#disallow-other-stacks=no',
+  }
 }

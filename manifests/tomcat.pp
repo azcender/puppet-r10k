8a80a032d0943745873b3a3721d5e93c82b58535
Bryan Belanger <bbelanger@azcender.com>
2014-06-26 14:31:00 -0400

    Added temo file resource creation to tomcat.pp

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -1,6 +1,11 @@
 # Profile for a tomcat application instance
 # Should be extended to new classes for other app servers
 class profile::tomcat {
+  $files = hiera('profile::tomcat::file')
+
+  # Create file resources
+  create_resource(file, $files)
+
   include java
 
   class{ '::tomcat': }

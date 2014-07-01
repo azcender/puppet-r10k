b81e59351f6dfefadb280d1e9c9b06deb31847a0
Bryan Belanger <bbelanger@azcender.com>
2014-06-30 22:15:54 -0400

    Created init settings

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,35 @@
 # Does basic setup for all profiles
 # Puppet master DOES NOT inherit from this
 class profile {
-  $puppet_agent_environment = hiera('profile::puppet_agent_environment', 'production')
+  # Puppet agent dev environment
+  # Default: production
+  $puppet_agent_environment =
+    hiera('profile::puppet_agent_environment', 'production')
 
-  # Create file resources
-  create_resources(ini_setting, $puppet_agent_environment)
+  # Puppet agent polling interval
+  # Default:  1800 (30 mins)
+  $puppet_agent_runinterval =
+    hiera('profile::puppet_agent_runinterval', '1800')
+
+  # Initialization value for puppet development environment:
+  #    production
+  #    staging
+  #    dev
+  #    other (Often names for user id)
+  ini_setting { 'set puppet development environment':
+    ensure   => present,
+    path     => '/etc/puppetlabs/puppet/puppet.conf',
+    section  => 'agent',
+    setting  => 'environment',
+    value    => $puppet_agent_environment,
+  }
+
+  # Set agent polling interval
+  ini_setting { 'set puppet agent polling interval':
+    ensure   => present,
+    path     => '/etc/puppetlabs/puppet/puppet.conf',
+    section  => 'main',
+    setting  => 'runinterval',
+    value    => $puppet_agent_runinterval,
+  }
 }

73df2d8f8cb22fdc8fdecfc0f6db7fd2be35fdfa
Bryan Belanger <bbelanger@azcender.com>
2014-07-01 16:34:30 -0400

    Changed java_web_application_server to use instance

--- manifests/java_web_application_server.pp
+++ manifests/java_web_application_server.pp
@@ -20,5 +20,5 @@ class profile::java_web_application_server {
   # The applications are configured in hiera
   $applications = hiera('profile::java_web_application_server::applications')
 
-  create_resources('java_web_application_server', $applications)
+  create_resources('java_web_application_server::instance', $applications)
 }

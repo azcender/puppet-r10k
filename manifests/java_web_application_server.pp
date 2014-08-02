5c98332628d6f5b0b022fcb447cd240220e25385
Bryan Belanger <bbelanger@azcender.com>
2014-08-01 23:42:07 -0400

    Now pull a full list of the orgs apps

--- manifests/java_web_application_server.pp
+++ manifests/java_web_application_server.pp
@@ -23,15 +23,24 @@ class profile::java_web_application_server {
   # instances.
   class{ '::tomcat': }
 
-  # Create the default tomcat libraries
+  # Create the default information needed to create an instance
+  #
+  # tomcat_libraries (hash):
+  #   The shared tomcat libraries needed for these instances
+  #
+  # applications (hash):
+  #   The applications available to these instances. Likely the entire list
+  #   of applications hosted by this organization. Only a fraction will likely
+  #   be hosted at any given time
   # 
   # Type: maven
   $applications_default = {
-    tomcat_libraries => hiera('java_web_application_server::tomcat_libraries')
+    tomcat_libraries       => hiera('profile::java_web_application_server::tomcat_libraries')
+    available_applications => hiera('profile::java_web_application_server::applications')
   }
 
-  # The applications are configured in hiera
-  $applications = hiera('profile::java_web_application_server::applications')
+  # The instances to be configured on this node
+  $instances = hiera('profile::java_web_application_server::instances')
 
-  create_resources('java_web_application_server::instance', $applications, $applications_default)
+  create_resources('java_web_application_server::instance', $instances, $applications_default)
 }

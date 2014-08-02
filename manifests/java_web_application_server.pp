4e80215ab3fee032c55e81f49d95eb1306958f03
Bryan Belanger <bbelanger@azcender.com>
2014-08-01 23:47:53 -0400

    Fixed hash format error

--- manifests/java_web_application_server.pp
+++ manifests/java_web_application_server.pp
@@ -35,8 +35,8 @@ class profile::java_web_application_server {
   # 
   # Type: maven
   $applications_default = {
-    tomcat_libraries       => hiera('profile::java_web_application_server::tomcat_libraries')
-    available_applications => hiera('profile::java_web_application_server::applications')
+    tomcat_libraries       => hiera('profile::java_web_application_server::tomcat_libraries'),
+    available_applications => hiera('profile::java_web_application_server::applications'),
   }
 
   # The instances to be configured on this node

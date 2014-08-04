e31dda1ab83a802d7697688a116e471e35100a29
Bryan Belanger <bbelanger@azcender.com>
2014-08-04 14:14:39 -0400

    Remove tomcat libraries lookup

--- manifests/java_web_application_server.pp
+++ manifests/java_web_application_server.pp
@@ -35,7 +35,6 @@ class profile::java_web_application_server {
   # 
   # Type: maven
   $applications_default = {
-    tomcat_libraries       => hiera('profile::java_web_application_server::tomcat_libraries'),
     available_applications => hiera('profile::java_web_application_server::applications'),
   }
 

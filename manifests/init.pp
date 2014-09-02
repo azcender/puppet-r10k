5d259090b4e51d6d6ecebf6b742ac73b229f1cb3
Paul Talbot <prolixalias@gmail.com>
2014-09-02 16:14:11 -0400

    adjusted path to puppet.conf on windows

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,7 +33,7 @@ class profile {
       # Set "environment"
       ini_setting { 'set puppet environment':
         ensure   => present,
-        path     => 'C:\Program Files (x86)\Puppet Labs\Puppet Enterprise\puppet\ext\puppet.conf',
+        path     => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
         section  => 'agent',
         setting  => 'environment',
         value    => $agent_environment,
@@ -42,16 +42,16 @@ class profile {
       # Set agent polling interval
       ini_setting { 'set puppet agent polling interval':
         ensure   => present,
-        path     => 'C:\Program Files (x86)\Puppet Labs\Puppet Enterprise\puppet\ext\puppet.conf',
+        path     => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
         section  => 'main',
         setting  => 'runinterval',
         value    => $runinterval,
       }
 
-      # Set http listener on
+      # Set HTTP listener on
       ini_setting { 'set http api listener on':
         ensure   => present,
-        path     => 'C:\Program Files (x86)\Puppet Labs\Puppet Enterprise\puppet\ext\puppet.conf',
+        path     => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
         section  => 'agent',
         setting  => 'listen',
         value    => true,
@@ -76,7 +76,7 @@ class profile {
         value    => $runinterval,
       }
 
-      # Set http listener on
+      # Set HTTP listener on
       ini_setting { 'set http api listener on':
         ensure   => present,
         path     => '/etc/puppetlabs/puppet/puppet.conf',

b28d6aef7eb494f6b9b5a96f1ee81ce39226578d
Bryan Belanger <bbelanger@azcender.com>
2014-09-07 21:20:26 -0400

    Move to puppetlabse tomcat module

--- manifests/java_web_application_server.pp
+++ manifests/java_web_application_server.pp
@@ -6,35 +6,10 @@ class profile::java_web_application_server {
   # Java is needed to run the applications¬
   include java
 
-  # We use Maven to deploy applications
-
-  # Get any maven repositories we need to search for packaging
-  $maven_repos = hiera('profile::java_web_application_server::maven_repos')
-
-  # The maven class requires a version number
-  $maven_version = hiera('profile::java_web_application_server::maven_version')
-
-  # Retrieve the shared libraries
-  $shared_libraries = hiera('profile::java_web_application_server::shared_libraries')
-
-  notice("$shared_libraries")
-
-  # Install Maven
-  class { "maven::maven":
-    version => $maven_version, # version to install
-  }
-
   # A standard tomcat instace needs to be instantiated before building separate
   # instances.
   class{ '::tomcat': }
-
-  # Add repos to maven default
-  $maven_default = {
-    repos => hiera('profile::java_web_application_server::maven_repos'),
-  }
-
-   # Create the shared libraries using the facade object
-  create_resources('::java_web_application_server::maven', $shared_libraries, $maven_default)
+  class{ '::java': }
 
   # Create the default information needed to create an instance
   #
@@ -50,7 +25,6 @@ class profile::java_web_application_server {
   $applications_default = {
     available_applications => hiera('profile::java_web_application_server::applications'),
     available_resources    => hiera('profile::java_web_application_server::resources'),
-    repos                  => hiera('profile::java_web_application_server::maven_repos'),
   }
 
   # The instances to be configured on this node

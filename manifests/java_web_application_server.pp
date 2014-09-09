5e4dd1fd72394abb94fbb408005738ed168318f7
Bryan Belanger <bbelanger@azcender.com>
2014-09-09 09:43:46 -0400

    Removed app and resource array lookups from jwas profile

--- manifests/java_web_application_server.pp
+++ manifests/java_web_application_server.pp
@@ -18,22 +18,6 @@ class profile::java_web_application_server {
     ensure => directory,
   }
 
-  # Create the default information needed to create an instance
-  #
-  # tomcat_libraries (hash):
-  #   The shared tomcat libraries needed for these instances
-  #
-  # applications (hash):
-  #   The applications available to these instances. Likely the entire list
-  #   of applications hosted by this organization. Only a fraction will likely
-  #   be hosted at any given time
-  # 
-  # Type: maven
-  $applications_default = {
-    available_applications => hiera('profile::java_web_application_server::applications'),
-    available_resources    => hiera('profile::java_web_application_server::resources'),
-  }
-
   # The instances to be configured on this node
   $instances = hiera('profile::java_web_application_server::instances')
 

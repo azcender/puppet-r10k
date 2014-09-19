af3d4e2009e580ce7fd25736df46237142e174a4
Paul Talbot <prolixalias@gmail.com>
2014-09-19 12:58:23 -0400

    adding test chocolatey package

--- manifests/chocolatey.pp
+++ manifests/chocolatey.pp
@@ -1,4 +1,10 @@
 # Setup default package provider to chocolatey on Windows nodes
 class profile::chocolatey inherits profile {
   Package { provider => chocolatey }
+
+  Package {'git':
+    ensure => latest,
+    provider => chocolatey,
+  }
+
 }

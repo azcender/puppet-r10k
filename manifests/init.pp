dfe235ce813bbaddaea2c1a225bb400e07c56d5e
Paul Talbot <prolixalias@gmail.com>
2014-10-01 10:46:55 -0400

    added host entry for localhost

--- manifests/init.pp
+++ manifests/init.pp
@@ -84,6 +84,11 @@ class profile {
     value    => true,
   }
 
+  host { 'localhost':
+    ip => '127.0.0.1',
+    host_aliases => [ "${clientcert}", "${fqdn}" ],
+  }
+
   # Create defined files
   $files = hiera_hash('files', {})
 

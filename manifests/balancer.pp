6eac4754a6326c7e1c637c2d7405170e040300e1
Bryan Belanger <bbelanger@azcender.com>
2014-10-02 13:54:45 -0400

    Move mods to balance profile

--- manifests/balancer.pp
+++ manifests/balancer.pp
@@ -1,9 +1,13 @@
 # A wrapper that contains all thr functionality needed for a standard java web
 # application.
 # Does not support JEE applications
-class profile::balanc:wer inherits profile {
+class profile::balancer inherits profile {
   # The load balancer runs from the puppet apache module
   include ::apache
+
+  # Apache submodules required for proxy
+  apache::mod { 'proxy_ajp': }¬
+  apache::mod { 'proxy_html': }¬
 
   # Get the vhost balancers to create
   $vhosts = hiera('profile::balancer::vhosts')

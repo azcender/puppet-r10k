e6cecadeafc1e3bc0bd79645a1bb3fe33a9059bd
Paul Talbot <prolixalias@gmail.com>
2014-10-16 14:49:02 -0400

    cleaned up

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,24 +1,12 @@
 # Profile for the Puppet master node(s)
 class profile::master {
+
   # Set global file settings
   file {'/etc/puppetlabs/puppet/modules':
     mode    => 'a+r',
     recurse => 'true'
   }
 
-#  file {'/etc/puppetlabs/puppet/environments':
-#    mode    => 'u+r',
-#    owner   => 'pe-puppet',
-#    group   => 'pe-puppet',
-#    recurse => 'true'
-#  }
-
-#  file {'/etc/puppetlabs/puppet/hiera':
-#    mode    => 'u+r',
-#    owner   => 'pe-puppet',
-#    group   => 'pe-puppet',
-#    recurse => 'true'
-#  }->
   file { '/etc/puppetlabs/puppet/hiera.yaml':
     ensure => 'file',
     owner  => 'root',

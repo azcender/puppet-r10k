5414158cddf8c16ef195f0b540cc20313eaeb25b
Bryan Belanger <bjbelanger@fs.fed.us>
2014-10-30 16:15:37 -0400

    Dynamic apache version added to jwas

--- manifests/java_web_application_server.pp
+++ manifests/java_web_application_server.pp
@@ -18,10 +18,14 @@ class profile::java_web_application_server inherits profile {
     require => File['/opt/staging'],
   }
 
-  file { '/opt/staging/tomcat/apache-tomcat-8.0.14.tar.gz':
+  $source_url = hiera('profile:java_web_application_server::source_url')
+
+  $apache_file = regsubst($source_url, '.*/(.*)', '\1')
+
+  file { "/opt/staging/tomcat/${apache_file}" :
     ensure  => file,
     mode    => 'ug=rw,o=r',
-    require => ::Staging::File['apache-tomcat-8.0.14.tar.gz'],
+    require => ::Staging::File[$apache_file],
   }
 
 
@@ -33,7 +37,7 @@ class profile::java_web_application_server inherits profile {
   # Default tomcat home to catalina_home
   $instances_default = {
     instance_basedir => hiera('tomcat::catalina_home'),
-    source_url       => hiera('profile:java_web_application_server::source_url'),
+    source_url       => $source_url,
   }
 
   # The instances to be configured on this node

3955f1b90d0907f73836cf04bcc27f8cba8ffea5
Bryan Belanger <bjbelanger@fs.fed.us>
2014-10-30 16:18:29 -0400

    Roll back balacner profile

--- manifests/balancer.pp
+++ manifests/balancer.pp
@@ -5,13 +5,7 @@ class profile::balancer inherits profile {
   # The load balancer runs from the puppet apache module
   include ::apache
 
-  file { '/etc/httpd/conf.d/proxy.load':
-    ensure  => file,
-    content => 'LoadModule proxy_module modules/mod_proxy.so',
-  }
-
   # Apache submodules required for proxy
-  apache::mod { 'proxy': }
   apache::mod { 'proxy_ajp': }
   apache::mod { 'proxy_html': }
 

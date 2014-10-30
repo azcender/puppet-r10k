23c79ae806fe7a0a6a439943d7107ed59914f9e4
Bryan Belanger <bjbelanger@fs.fed.us>
2014-10-30 15:45:47 -0400

    Force proxy.load for 19

--- manifests/balancer.pp
+++ manifests/balancer.pp
@@ -5,6 +5,11 @@ class profile::balancer inherits profile {
   # The load balancer runs from the puppet apache module
   include ::apache
 
+  file { '/etc/httpd/conf.d/proxy.load':
+    ensure  => file,
+    content => 'LoadModule proxy_module modules/mod_proxy.so',
+  }
+
   # Apache submodules required for proxy
   apache::mod { 'proxy': }
   apache::mod { 'proxy_ajp': }

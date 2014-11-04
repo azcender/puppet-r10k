04a840a043a7cf2d753fb43776e737108aba93c3
Ron Compos <rcompos@gmail.com>
2014-11-04 12:47:52 -0700

    Added file copy of PHP index page and validation script.

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -3,9 +3,25 @@
 
 class profile::lamp inherits profile {
 
-  include ::apache
-  include ::apache::mod::php
-  include ::mysql::server
-  include ::mysql::bindings::php
+   include ::apache
+   include ::apache::mod::php
+   include ::mysql::server
+   include ::mysql::bindings::php
+
+   file { "/var/www/html/index.php":
+      owner => "root",
+      group => "root",
+      mode  => 0644,
+      replace => "false",
+      source => "puppet:///files/lamp_html/index.php"
+   }
+
+   file { "/var/www/html/lamp.php":
+      owner => "root",
+      group => "root",
+      mode  => 0644,
+      replace => "false",
+      source => "puppet:///files/lamp_html/lamp.php"
+   }
 
 }

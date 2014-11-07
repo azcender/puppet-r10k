02d9c4207706c7836de39d8e66af80685930766c
Ron Compos <rcompos@gmail.com>
2014-11-07 13:55:19 -0700

    Adjusted to work with new module lamp.

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -1,27 +1,4 @@
-# A wrapper that contains all thr functionality needed for a standard LAMP 
-# server (Linux Apache MySQL PHP).
-
+# Profile for standard LAMP (Linux Apache MySQL PHP) server.
 class profile::lamp inherits profile {
-
-   include ::apache
-   include ::apache::mod::php
-   include ::mysql::server
-   include ::mysql::bindings::php
-
-   file { "/var/www/html/index.php":
-      owner => "root",
-      group => "root",
-      mode  => 0644,
-      replace => "false",
-      source => "puppet:///files/lamp_html/index.php"
-   }
-
-   file { "/var/www/html/lamp.php":
-      owner => "root",
-      group => "root",
-      mode  => 0644,
-      replace => "false",
-      source => "puppet:///files/lamp_html/lamp.php"
-   }
-
+   include ::lamp
 }

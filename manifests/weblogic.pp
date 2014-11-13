2e5b660c155387ce2103bf0e2a2a815351606f98
Bryan Belanger <bjbelanger@fs.fed.us>
2014-11-12 22:27:06 -0500

    Watered down wl

---
+++ manifests/weblogic.pp
@@ -0,0 +1,26 @@
+# A wrapper that contains all thr functionality needed for a standard java web
+# application --- does not support JEE applications
+
+class profile::weblogic {
+  include ::profile
+  include ::java
+#  include ::orawls::weblogic
+#  include ::orautils
+
+  group { 'dba' :
+    ensure => present,
+  }
+
+  user { 'oracle' :
+    ensure     => present,
+    groups     => 'dba',
+    shell      => '/bin/bash',
+    password   => 'password1',
+    home       => "/home/oracle",
+    comment    => 'Oracle oracle user created by Puppet',
+    managehome => true,
+    require    => Group['dba'],
+  }
+
+#  Class['java'] -> Class['orawls::weblogic']
+}

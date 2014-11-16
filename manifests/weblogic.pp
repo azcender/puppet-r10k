484111e2958e381cf6b0923ceee66a10794bdcc7
Bryan Belanger <bbelanger@azcender.com>
2014-11-15 19:43:11 -0500

    Put back jdk7 call

--- manifests/weblogic.pp
+++ manifests/weblogic.pp
@@ -47,28 +47,28 @@ class os {
 
 }
 
-#class java {
-#  require os
-#
-#  $remove = [ "java-1.7.0-openjdk.x86_64", "java-1.6.0-openjdk.x86_64" ]
-#
-#  package { $remove:
-#    ensure  => absent,
-#  }
-#
-#  include jdk7
-#
-#  jdk7::install7{ 'jdk1.7.0_55':
-#      version                   => "7u55" ,
-#      fullVersion               => "jdk1.7.0_55",
-#      alternativesPriority      => 18000,
-#      x64                       => true,
-#      downloadDir               => "/var/tmp/install",
-#      urandomJavaFix            => true,
-#      rsakeySizeFix             => true,
-#      sourcePath                => "/software",
-#  }
-#}
+class java {
+  require os
+
+  $remove = [ "java-1.7.0-openjdk.x86_64", "java-1.6.0-openjdk.x86_64" ]
+
+  package { $remove:
+    ensure  => absent,
+  }
+
+  include jdk7
+
+  jdk7::install7{ 'jdk1.7.0_55':
+      version                   => "7u55" ,
+      fullVersion               => "jdk1.7.0_55",
+      alternativesPriority      => 18000,
+      x64                       => true,
+      downloadDir               => "/var/tmp/install",
+      urandomJavaFix            => true,
+      rsakeySizeFix             => true,
+      sourcePath                => "/software",
+  }
+}
 
 
 class fmw{

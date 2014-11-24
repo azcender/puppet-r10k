2d2cfa25a5201941c0d68d32ce34c594324df59d
Ron Compos <rcompos@gmail.com>
2014-11-24 11:01:10 -0700

    Moved firewall change to profile instead of module.

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -1,6 +1,13 @@
 # Profile for standard LAMP (Linux Apache MySQL PHP) server.
 class profile::lamp {
-  include ::profile
 
-  include ::lamp
+   include ::profile
+   include ::lamp
+
+   firewall { '100 allow http and https access':
+     port   => [80, 443],
+     proto  => tcp,
+     action => accept,
+   }
+
 }

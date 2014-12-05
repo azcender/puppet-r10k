f34650e6d87c445aeb2b6eeb2eec17e3e23f1e38
Bryan Belanger <bbelanger@azcender.com>
2014-12-05 15:43:32 -0500

    Test JM

--- manifests/jenkins_master.pp
+++ manifests/jenkins_master.pp
@@ -12,37 +12,37 @@ class profile::jenkins_master {
   # The FS Jenkins requires a git executable
   include ::git
 
-#  $private_key = hiera('profile::jenkins_master::private_key')
-#  $public_key = hiera('profile::jenkins_master::public_key')
+  $private_key = hiera('profile::jenkins_master::private_key', undef)
+  $public_key = hiera('profile::jenkins_master::public_key', undef)
 
   # Ensure the private and public key are installed in home
 
   # Jenkins SSH directory
-#  file { '/var/lib/jenkins/.ssh':
-#    ensure  => directory,
-#    mode    => '0700',
-#    owner   => 'jenkins',
-#    group   => 'jenkins',
-#    require => User['jenkins'],
-#  }
+  file { '/var/lib/jenkins/.ssh':
+    ensure  => directory,
+    mode    => '0700',
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
 
   # Jenkins RSA private key
-#  file { '/var/lib/jenkins/.ssh/id_rsa':
-#    ensure  => file,
-#    mode    => '0600',
-#    owner   => 'jenkins',
-#    group   => 'jenkins',
-#    content => $private_key,
-#    require => File['/var/lib/jenkins/.ssh'],
-#  }
+  file { '/var/lib/jenkins/.ssh/id_rsa':
+    ensure  => file,
+    mode    => '0600',
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    content => $private_key,
+    require => File['/var/lib/jenkins/.ssh'],
+  }
 
   # Jenkins RSA public key
-#  file { '/var/lib/jenkins/.ssh/id_rsa.pub':
-#    ensure  => file,
-#    mode    => '0644',
-#    owner   => 'jenkins',
-#    group   => 'jenkins',
-#    content => $public_key,
-#    require => File['/var/lib/jenkins/.ssh'],
-#  }
+  file { '/var/lib/jenkins/.ssh/id_rsa.pub':
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    content => $public_key,
+    require => File['/var/lib/jenkins/.ssh'],
+  }
 }

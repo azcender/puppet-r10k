5b29f9c154ecbaa14162809a32b1a612cb3d63db
Bryan Belanger <bbelanger@azcender.com>
2014-12-23 00:17:55 -0500

    Empty tomcat container

---
+++ manifests/tomcat_container.pp
@@ -0,0 +1,5 @@
+# A wrapper that contains all thr functionality needed for a standard java web
+# application --- does not support JEE applications
+class profile::tomcat_container {
+  #  include ::tomcat
+}

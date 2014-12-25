ea2b2fb8b60ccdd38c14c7372f18b6409fe138e2
Bryan Belanger <bbelanger@azcender.com>
2014-12-24 23:04:00 -0500

    Add war file

--- manifests/tomcat_container.pp
+++ manifests/tomcat_container.pp
@@ -2,4 +2,8 @@
 # application --- does not support JEE applications
 class profile::tomcat_container {
   #  include ::tomcat
+  ::tomcat::war { 'sample':
+    catalina_base => '/usr/local/tomcat',
+    war_source    => 'http://artifactory.azcender.com/artifactory/ext-release-local/org/apache/tomcat/sample/1.0.0/sample-1.0.0.war',
+  }
 }

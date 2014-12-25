66bef96d39cbd45b6457057ae7e06337c4eec4d3
Bryan Belanger <bbelanger@azcender.com>
2014-12-24 23:38:06 -0500

    Fix resource name

--- manifests/tomcat_container.pp
+++ manifests/tomcat_container.pp
@@ -2,7 +2,7 @@
 # application --- does not support JEE applications
 class profile::tomcat_container {
   #  include ::tomcat
-  ::tomcat::war { 'sample':
+  ::tomcat::war { 'sample.war':
     catalina_base => '/usr/local/tomcat',
     war_source    => 'http://artifactory.azcender.com/artifactory/ext-release-local/org/apache/tomcat/sample/1.0.0/sample-1.0.0.war',
   }

f9b9c6d1b258a5ecfa3910a5f9cbb60d6f1d4c61
Bryan Belanger <bbelanger@azcender.com>
2015-01-18 20:05:37 -0500

    new tomcat profile

---
+++ manifests/tomcat.pp
@@ -0,0 +1,67 @@
+# A wrapper that contains all thr functionality needed for a standard java web
+# application --- does not support JEE applications
+
+class profile::tomcat {
+  include ::profile
+
+  # Java is needed to run the applications
+  # A standard tomcat instace needs to be instantiated before building separate
+  # instances
+  include ::java
+  include ::tomcat
+
+  if ! $::groupid {
+    $_groupid = 'gov.fed.us'
+  } else {
+    $_groupid = $::groupid
+  }
+
+
+  # Hard fix of staging dirs
+  # TODO: Fix this
+  file { '/opt/staging/tomcat':
+    ensure  => directory,
+    mode    => 'a=rx',
+    require => File['/opt/staging'],
+  }
+
+  $source_url = hiera('profile::tomcat::source_url')
+
+  $apache_file = regsubst($source_url, '.*/(.*)', '\1')
+
+  file { "/opt/staging/tomcat/${apache_file}" :
+    ensure  => file,
+    mode    => 'ug=rw,o=r',
+    require => ::Staging::File[$apache_file],
+  }
+
+
+  # Since this uses wget to obtain the war files make the cache directory
+  file { '/var/cache/wget':
+    ensure => directory,
+  }
+
+  ::tomcat::instance { $name:
+    catalina_base => '/opt/tomcat',
+    source_url    => $source_url,
+  }
+
+  ::java_web_application_server::maven { $name:
+    ensure        => present,
+    war_name      => "${::application}.war",
+    groupid       => $_groupid,
+    artifactid    => $::application,
+    version       => $::version,
+    maven_repo    => 'http://artifactory.azcender.com/artifactory/ext-release-local',
+    catalina_base => '/opt/tomcat',
+    packaging     => 'war',
+    require       => [::Tomcat::Instance[$name]],
+  }
+
+  ::tomcat::service { $name:
+    service_name  => $name,
+    catalina_home => '/opt/tomcat',
+    catalina_base => '/opt/tomcat',
+    require       => [::Java_web_application_server::Maven[$name]],
+  }
+}

e47379b61b5ed6cfeff83d8ce6ed69cebc310d0c
Bryan Belanger <bbelanger@azcender.com>
2015-02-02 16:42:20 -0500

    New candy

--- lib/facter/candy.rb
+++ lib/facter/candy.rb
@@ -3,10 +3,11 @@ require 'json'
 
 Facter.add('candy') do
   setcode do
-    containers = []
-    inspections = []
-    interfaces = `docker ps`
-    #  interfaces = Facter.value(:interfaces)
+    containers  = []
+    inspections = {}
+
+    interfaces  = `docker ps`
+
     # the 'interfaces' fact returns a single comma-delimited string, e.g., "lo0,eth0,eth1"
     interfaces_array = interfaces.split("\n")
 
@@ -14,22 +15,30 @@ Facter.add('candy') do
 
     interfaces_array.each do |interface|
       containers << interface.split(' ')[0]
-
     end
 
     containers.each do |container|
       inspection = {}
       nextEntry = {}
-      #ip = `docker inspect --format='{{.NetworkSettings.IPAddress}}' #{container}`
+      _ports = []
+
       ip = `docker inspect #{container}`
       nextEntry =  JSON.parse(ip)
 
-      inspection["Hostname"] = nextEntry[0]["Config"]["Hostname"]
-      inspection["IPAddress"] = nextEntry[0]["NetworkSettings"]["IPAddress"]
-      inspection["Ports"] = nextEntry[0]["NetworkSettings"]["Ports"]
+      ports = nextEntry[0]["NetworkSettings"]["Ports"]
+
+      ports.each do |port|
+        _ports << port[0].split('/')[0]
+      end
+
+      inspection["server_names"] = nextEntry[0]["Config"]["Hostname"]
+      inspection["ipaddresses"]  = nextEntry[0]["NetworkSettings"]["IPAddress"]
+      inspection["ports"]        = _ports
+      # inspection["name"]         = nextEntry[0]["Config"]["Hostname"]
 
 
-      inspections << inspection
+      # inspections << inspection
+      inspections[inspection["server_names"]] = inspection
     end
 
     inspections

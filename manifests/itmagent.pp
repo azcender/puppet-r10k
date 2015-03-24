44a0e58dd0bcc7a815e84a1c9c4529ac075bfae3
Ron Compos <rcompos@gmail.com>
2015-03-24 15:41:54 -0600

    Added params

--- manifests/itmagent.pp
+++ manifests/itmagent.pp
@@ -1,7 +1,11 @@
-# Profile for standard LAMP (Linux Apache MySQL PHP) server.
+# Profile for IBM Tivoli Monitoring (ITM) Agent
 class profile::itmagent {
 
    include ::profile
-   include ::itmagent
+   class { "itmagent":
+     itm_server => 'temsy.blacklab.lan',
+     nfs_host   => '10.20.1.8',
+     mnt_dir    => '/mnt/centos70s0'
+   }
 
 }

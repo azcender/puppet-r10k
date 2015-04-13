af5576e68ec82ad36e0b1a4721c1710fc260067a
Ron Compos <rcompos@gmail.com>
2015-04-13 10:29:22 -0600

    Fixed missing commas

--- manifests/itmagent.pp
+++ manifests/itmagent.pp
@@ -5,9 +5,9 @@ class profile::itmagent {
      itm_server     => 'temsy.blacklab.lan',
      nfs_host       => '10.20.1.8',
      mnt_dir        => '/mnt/centos70s0',
-     src_dir        => 'itm630agent_inst'
-     nfs_dir        => '/var/centos70s0'
-     dir_tmp        => '/tmp'
-     nfs_options    => '-t nfs'
+     src_dir        => 'itm630agent_inst',
+     nfs_dir        => '/var/centos70s0',
+     dir_tmp        => '/tmp',
+     nfs_options    => '-t nfs',
    }
 }

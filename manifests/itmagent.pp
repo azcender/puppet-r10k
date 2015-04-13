2032d3e756a940d7bbe1e52c9efdb7e29ff0df2e
Ron Compos <rcompos@gmail.com>
2015-04-13 09:42:04 -0600

    Test params.

--- manifests/itmagent.pp
+++ manifests/itmagent.pp
@@ -3,10 +3,10 @@ class profile::itmagent {
 
    include ::profile
    include ::itmagent
-   #class { '::itmagent':
-   #  itm_server => 'temsy.blacklab.lan',
-   #  nfs_host   => '10.20.1.8',
-   #  mnt_dir    => '/mnt/centos70s0',
-   #}
+   class { '::itmagent':
+     itm_server => 'temsy.blacklab.lan',
+     nfs_host   => '10.20.1.8',
+     mnt_dir    => '/mnt/centos70s0',
+   }
 
 }

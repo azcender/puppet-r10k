7b4dd6a8b10de56cd503d6c968ccddbf2da0c80c
Ron Compos <rcompos@gmail.com>
2015-04-13 10:27:05 -0600

    Added some params.

--- manifests/itmagent.pp
+++ manifests/itmagent.pp
@@ -1,12 +1,13 @@
 # Profile for IBM Tivoli Monitoring (ITM) Agent
 class profile::itmagent {
-
    include ::profile
-   include ::itmagent
    class { '::itmagent':
-     itm_server => 'temsy.blacklab.lan',
-     nfs_host   => '10.20.1.8',
-     mnt_dir    => '/mnt/centos70s0',
+     itm_server     => 'temsy.blacklab.lan',
+     nfs_host       => '10.20.1.8',
+     mnt_dir        => '/mnt/centos70s0',
+     src_dir        => 'itm630agent_inst'
+     nfs_dir        => '/var/centos70s0'
+     dir_tmp        => '/tmp'
+     nfs_options    => '-t nfs'
    }
-
 }

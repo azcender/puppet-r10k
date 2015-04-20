b2d143ec7b0f22657b17bf555b5c6558d10509a8
Paul Talbot <prolixalias@gmail.com>
2015-04-20 14:57:30 -0400

    using config from Icinga vagrant project for profile

--- manifests/icinga_server.pp
+++ manifests/icinga_server.pp
@@ -7,10 +7,169 @@ class profile::icinga_server(
   # Include base class
   include ::profile
 
-  # Include postgresql classes
-  include ::postgresql
-  include ::postgresql::server
+  include icinga_rpm
+  #include epel
+  #include '::mysql::server'
+  include '::postgresql::server'
+  include icinga2
+  include icinga2_ido_mysql
+  include icinga2-classicui
+  include icinga2-icinga-web
+  include icingaweb2
+  include icingaweb2-internal-db-mysql
+  include monitoring-plugins
+  include selinux
 
-  include ::icinga2
-  include ::icinga2::server
+  icingaweb2::module { [ 'businessprocess', 'pnp4nagios', 'generictts' ]:
+    builtin => false
+  }
+
+  ####################################
+  # Webserver
+  ####################################
+
+  class {'apache':
+    # don't purge php, icingaweb2, etc configs
+    purge_configs => false,
+  }
+
+  class {'::apache::mod::php': }
+
+  include '::php::cli'
+  include '::php::mod_php5'
+
+  php::ini { '/etc/php.ini':
+    display_errors => 'On',
+    memory_limit => '256M',
+    date_timezone => 'Europe/Berlin',
+    session_save_path => '/var/lib/php/session'
+  }
+
+  ####################################
+  # Start page at http://localhost/
+  ####################################
+
+  file { '/var/www/html/index.html':
+    source    => 'puppet:////vagrant/files/var/www/html/index.html',
+    owner     => 'apache',
+    group     => 'apache',
+    require   => Class['Apache']
+  }
+
+  file { '/var/www/html/icinga_wall.png':
+    source    => 'puppet:////vagrant/files/var/www/html/icinga_wall.png',
+    owner     => 'apache',
+    group     => 'apache',
+    require   => Class['Apache']
+  }
+
+  ####################################
+  # Misc
+  ####################################
+  # fix puppet warning.
+  # https://ask.puppetlabs.com/question/6640/warning-the-package-types-allow_virtual-parameter-will-be-changing-its-default-value-from-false-to-true-in-a-future-release/
+  if versioncmp($::puppetversion,'3.6.1') >= 0 {
+    $allow_virtual_packages = hiera('allow_virtual_packages',false)
+    Package {
+      allow_virtual => $allow_virtual_packages,
+    }
+  }
+
+  package { [ 'vim-enhanced', 'mailx', 'tree', 'gdb', 'rlwrap', 'git' ]:
+    ensure => 'installed'
+  }
+
+  package { 'bash-completion':
+    ensure => 'installed',
+    require => Class['epel']
+  }
+
+  @user { vagrant: ensure => present }
+  User<| title == vagrant |>{
+    groups +> ['icinga', 'icingacmd'],
+    require => Package['icinga2']
+  }
+
+  file { '/etc/motd':
+    source => 'puppet:////vagrant/files/etc/motd',
+    owner => root,
+    group => root
+  }
+
+  file { [ '/root/.vim',
+       '/root/.vim/syntax',
+       '/root/.vim/ftdetect' ] :
+    ensure    => 'directory'
+  }
+
+  exec { 'copy-vim-syntax-file':
+    path => '/bin:/usr/bin:/sbin:/usr/sbin',
+    command => 'cp -f /usr/share/doc/icinga2-common-$(rpm -q icinga2-common | cut -d\'-\' -f3)/syntax/vim/syntax/icinga2.vim /root/.vim/syntax/icinga2.vim',
+    require => [ Package['vim-enhanced'], Package['icinga2-common'], File['/root/.vim/syntax'] ] }
+
+  exec { 'copy-vim-ftdetect-file':
+    path => '/bin:/usr/bin:/sbin:/usr/sbin',
+    command => 'cp -f /usr/share/doc/icinga2-common-$(rpm -q icinga2-common | cut -d\'-\' -f3)/syntax/vim/ftdetect/icinga2.vim /root/.vim/ftdetect/icinga2.vim',
+    require => [ Package['vim-enhanced'], Package['icinga2-common'], File['/root/.vim/syntax'] ] }
+
+####################################
+# Firewall
+####################################
+
+define rh_firewall_add_port($zone, $port) {
+  exec { $title :
+    path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+    command => "firewall-cmd --permanent --zone=${zone} --add-port=${port}",
+    unless  => "firewall-cmd --zone ${zone} --list-ports | fgrep -q ${port}",
+    require => Package['firewalld'],
+    notify  => Service['firewalld'],
+  }
+}
+
+# firewall: TODO add support for other OS unlike CentOS7
+  case $operatingsystem {
+    centos, redhat: {
+      if $operatingsystemrelease =~ /^7.*/ {
+        package { 'firewalld':
+          ensure => installed
+        }
+        service { 'firewalld':
+          ensure => running,
+          enable => true,
+          hasstatus => true,
+          hasrestart => true,
+          require => Package['firewalld']
+        }
+
+        rh_firewall_add_port { 'iptables-http-80':
+          zone => 'public',
+          port => '80/tcp',
+        }
+      }
+    }
+  }
+
+####################################
+# Icinga 2 General
+####################################
+
+  # enable the command pipe
+  icinga2::feature { 'command': }
+    # present icinga2 in icingaweb2's module documentation
+    file { '/usr/share/icingaweb2/modules/icinga2':
+    ensure => 'directory',
+    require => Package['icingaweb2']
+  }
+
+  file { '/usr/share/icingaweb2/modules/icinga2/doc':
+    ensure => 'link',
+    target => '/usr/share/doc/icinga2/markdown',
+    require => [ Package['icinga2'], Package['icingaweb2'], File['/usr/share/icingaweb2/modules/icinga2'] ], }
+
+  file { '/etc/icingaweb2/enabledModules/icinga2':
+    ensure => 'link',
+    target => '/usr/share/icingaweb2/modules/icinga2',
+    require => File['/etc/icingaweb2/enabledModules'],
+  }
+
 }

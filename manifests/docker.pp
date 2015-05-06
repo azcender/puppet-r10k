516042c63b8a907ab2f4e0477764ae79ba530bd4
Bryan Belanger <bbelanger@azcender.com>
2015-05-05 21:31:33 -0400

    Strip down docker profile to base class

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -4,29 +4,28 @@ class profile::docker {
   include ::profile
 
   include ::docker
-  include ::haproxy
 
-  file { '/etc/puppetlabs/facter':
-    ensure => directory,
-  }
+  #  file { '/etc/puppetlabs/facter':
+  #  ensure => directory,
+  #}
 
-  file { '/etc/puppetlabs/facter/facts.d':
-    ensure  => directory,
-    require => File['/etc/puppetlabs/facter'],
-  }
+  #file { '/etc/puppetlabs/facter/facts.d':
+  #  ensure  => directory,
+  #  require => File['/etc/puppetlabs/facter'],
+  #}
 
-  file {'/etc/puppetlabs/facter/facts.d/containers.rb':
-    ensure  => file,
-    source  => 'puppet:///modules/profile/containers.rb',
-    require => File['/etc/puppetlabs/facter/facts.d'],
-  }
+  #file {'/etc/puppetlabs/facter/facts.d/containers.rb':
+  #  ensure  => file,
+  #  source  => 'puppet:///modules/profile/containers.rb',
+  #  require => File['/etc/puppetlabs/facter/facts.d'],
+  #}
 
-  ::haproxy::listen { 'puppet00':
+  #::haproxy::listen { 'puppet00':
     #ipaddres => $::ipaddress,
-    mode      => 'http',
-    ipaddress => '*',
-    ports     => '8140',
-  }
+    #  mode      => 'http',
+    #ipaddress => '*',
+    #ports     => '8140',
+    #}
 
   #  ::haproxy::balancermember { '70b223b40eab':
   #  listening_service => 'puppet00',
@@ -35,32 +34,33 @@ class profile::docker {
   #  ports             => '8080',
   #}
 
-  $balancermember_defaults = {
-    listening_service => 'puppet00',
-    require           => [Class['::docker'], File['/etc/puppetlabs/facter/facts.d/containers.rb']],
-  }
+  #$balancermember_defaults = {
+  #  listening_service => 'puppet00',
+  #  require           =>
+  #[Class['::docker'], File['/etc/puppetlabs/facter/facts.d/containers.rb']],
+  #}
 
   # Create balance members if containers exist
-  create_resources('::haproxy::balancermember', $::containers,
-  $balancermember_defaults)
+  #create_resources('::haproxy::balancermember', $::containers,
+  #$balancermember_defaults)
 
   # Pull images
   #  $images_defaults = {
   #  before => Class['::haproxy::balancermember'],
   #}
 
-  ::Docker::Image <<||>> -> ::Haproxy::Balancermember <<||>>
-  ::Docker::Run <<||>> -> ::Haproxy::Balancermember <<||>>
+  #::Docker::Image <<||>> -> ::Haproxy::Balancermember <<||>>
+  #::Docker::Run <<||>> -> ::Haproxy::Balancermember <<||>>
 
-  $images = hiera('profile::docker::images', {})
+  #$images = hiera('profile::docker::images', {})
 
-  create_resources('::docker::image', $images)
+  #create_resources('::docker::image', $images)
 
   #  $runs_defaults = {
   #  before => Class['::haproxy::balancermember'],
   #}
 
-  $runs = hiera('profile::docker::runs', {})
+  #$runs = hiera('profile::docker::runs', {})
 
-  create_resources('::docker::run', $runs)
+  #create_resources('::docker::run', $runs)
 }

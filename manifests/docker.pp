2406af5ff19b1896c207b7d8325f4351c316460a
Bryan Belanger <bbelanger@azcender.com>
2015-05-07 11:03:38 -0400

    Docker profile wiht hard coded export haproxy

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -10,62 +10,10 @@ class profile::docker {
 
   create_resources('::docker::run', $runs)
 
-  #  file { '/etc/puppetlabs/facter':
-  #  ensure => directory,
-  #}
-
-  #file { '/etc/puppetlabs/facter/facts.d':
-  #  ensure  => directory,
-  #  require => File['/etc/puppetlabs/facter'],
-  #}
-
-  #file {'/etc/puppetlabs/facter/facts.d/containers.rb':
-  #  ensure  => file,
-  #  source  => 'puppet:///modules/profile/containers.rb',
-  #  require => File['/etc/puppetlabs/facter/facts.d'],
-  #}
-
-  #::haproxy::listen { 'puppet00':
-    #ipaddres => $::ipaddress,
-    #  mode      => 'http',
-    #ipaddress => '*',
-    #ports     => '8140',
-    #}
-
-  #  ::haproxy::balancermember { '70b223b40eab':
-  #  listening_service => 'puppet00',
-  #  server_names      => '70b223b40eab',
-  #  ipaddresses       => '172.17.0.2',
-  #  ports             => '8080',
-  #}
-
-  #$balancermember_defaults = {
-  #  listening_service => 'puppet00',
-  #  require           =>
-  #[Class['::docker'], File['/etc/puppetlabs/facter/facts.d/containers.rb']],
-  #}
-
-  # Create balance members if containers exist
-  #create_resources('::haproxy::balancermember', $::containers,
-  #$balancermember_defaults)
-
-  # Pull images
-  #  $images_defaults = {
-  #  before => Class['::haproxy::balancermember'],
-  #}
-
-  #::Docker::Image <<||>> -> ::Haproxy::Balancermember <<||>>
-  #::Docker::Run <<||>> -> ::Haproxy::Balancermember <<||>>
-
-  #$images = hiera('profile::docker::images', {})
-
-  #create_resources('::docker::image', $images)
-
-  #  $runs_defaults = {
-  #  before => Class['::haproxy::balancermember'],
-  #}
-
-  #$runs = hiera('profile::docker::runs', {})
-
-  #create_resources('::docker::run', $runs)
+  @@::haproxy::balancermember { $::ipaddress :
+    listening_service => 'docker',
+    server_names      => $::hostname,
+    ipaddresses       => $::ipaddress,
+    ports             => '8888',
+  }
 }

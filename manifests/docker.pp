1db9a9320af5e0b6d2105eddf1e2cfc6935a3d10
Paul Talbot <paul@fullfrontalingenuity.com>
2015-05-08 20:05:19 -0400

    formatting

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -1,13 +1,13 @@
 # Installs the Docker daemon
+#
+
 class profile::docker {
   # Include base class
   include ::profile
-
   include ::docker
 
   # Docker runs in docker yaml
   $runs = hiera('profile::docker::runs')
-
   create_resources('::docker::run', $runs)
 
   @@::haproxy::balancermember { $::ipaddress_ens33 :

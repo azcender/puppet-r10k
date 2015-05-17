bfbe652f5665f2bdce5dd2d790bac7b1ab7f1809
Bryan Belanger <bbelanger@azcender.com>
2015-05-16 22:30:43 -0400

    Strip base profile of init settings and auth settings

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,30 +4,6 @@
 #
 
 class profile {
-
-  # TODO: document this
-  $required_files = hiera_hash('sanity::file_contents::files', {})
-  $required_file_lines = hiera_hash('sanity::file_contents::file_lines', {})
-  $required_filesystems_mounts = hiera_hash('sanity::filesystems::mounts', {})
-  #class { 'sanity':
-  #  files             => $required_files,
-  #  file_lines        => $required_file_lines,
-  #  filesystem_mounts => $required_filesystems_mounts,
-  #}
-
-  $run_path   = "set /files${::confdir}/auth.conf/path[. = '/run'] /run"
-  $run_auth   = "set /files${::confdir}/auth.conf/path[. = '/run']/auth any"
-  $run_method =
-  "set /files${::confdir}/auth.conf/path[. = '/run']/method/1 save"
-  $run_allow  = "set /files${::confdir}/auth.conf/path[. = '/run']/allow/1 *"
-
-  $remove_root = "rm ${::confdir}/auth.conf/path[. = '/']"
-
-  # Add puppet auth entry for run
-  augeas { 'auth.conf':
-    changes => [$run_path, $run_auth, $run_method, $run_allow, $remove_root],
-  }
-
   # Puppet agent dev environment
   # Default: production
   $agent_environment =
@@ -37,48 +13,48 @@ class profile {
   # Default:  1800 (30 mins)
   $runinterval = hiera('profile::puppet_agent_runinterval', '1800')
 
-  ini_setting { 'remove bogus production env from main section if present':
-    ensure  => absent,
-    path    => "${::confdir}/puppet.conf",
-    section => 'main',
-    setting => 'environment',
-    value   => 'production',
-  }
+  #ini_setting { 'remove bogus production env from main section if present':
+  #  ensure  => absent,
+  #  path    => "${::confdir}/puppet.conf",
+  #  section => 'main',
+  #  setting => 'environment',
+  #  value   => 'production',
+  #}
 
-  ini_setting { 'set puppet development environment':
-    ensure  => present,
-    path    => "${::confdir}/puppet.conf",
-    section => 'agent',
-    setting => 'environment',
-    value   => $agent_environment,
-  }
+  #  ini_setting { 'set puppet development environment':
+  #  ensure  => present,
+  #  path    => "${::confdir}/puppet.conf",
+  #  section => 'agent',
+  #  setting => 'environment',
+  #  value   => $agent_environment,
+  #}
 
   # Set agent polling interval
-  ini_setting { 'set puppet agent polling interval':
-    ensure  => present,
-    path    => "${::confdir}/puppet.conf",
-    section => 'main',
-    setting => 'runinterval',
-    value   => $runinterval,
-  }
+  #ini_setting { 'set puppet agent polling interval':
+  #  ensure  => present,
+  #  path    => "${::confdir}/puppet.conf",
+  #  section => 'main',
+  #  setting => 'runinterval',
+  #  value   => $runinterval,
+  #}
 
   # Set HTTP listener on
-  ini_setting { 'set http api listener on':
-    ensure  => present,
-    path    => "${::confdir}/puppet.conf",
-    section => 'agent',
-    setting => 'listen',
-    value   => true,
-  }
+  #ini_setting { 'set http api listener on':
+  #  ensure  => present,
+  #  path    => "${::confdir}/puppet.conf",
+  #  section => 'agent',
+  #  setting => 'listen',
+  #  value   => true,
+  #}
 
   # Enable pluginsync
-  ini_setting { 'enable pluginsync':
-    ensure  => present,
-    path    => "${::confdir}/puppet.conf",
-    section => 'main',
-    setting => 'pluginsync',
-    value   => true,
-  }
+  #ini_setting { 'enable pluginsync':
+  #  ensure  => present,
+  #  path    => "${::confdir}/puppet.conf",
+  #  section => 'main',
+  #  setting => 'pluginsync',
+  #  value   => true,
+  #}
 
   # Create defined files
   $files = hiera_hash('files', {})

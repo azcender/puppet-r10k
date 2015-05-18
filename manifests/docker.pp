8cf2711aef7725e4261ba21a864ce2a1e513e74d
Bryan Belanger <bbelanger@azcender.com>
2015-05-18 10:36:46 -0400

    PE 3.8 not ready for future parser

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -8,18 +8,15 @@ class profile::docker {
 
   # Docker runs in docker yaml
   $runs = hiera('profile::docker::runs')
+
   create_resources('::docker::run', $runs)
 
-  $runs.each |$run| {
-    $concat_name = "${name}-${::ipaddress}"
+  $concat_name = "${name}-${::ipaddress}"
 
-    notice($run)
-
-    @@::haproxy::balancermember { $concat_name:
-      listening_service => 'docker',
-      server_names      => $::hostname,
-      ipaddresses       => $::ipaddress,
-      ports             => '8888',
-    }
+  @@::haproxy::balancermember { $concat_name:
+    listening_service => 'docker',
+    server_names      => $::hostname,
+    ipaddresses       => $::ipaddress,
+    ports             => '8888',
   }
 }

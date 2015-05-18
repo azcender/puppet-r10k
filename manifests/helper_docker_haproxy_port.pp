1f421113464e55e0ed9d97e54f67af9cfa312788
Bryan Belanger <bbelanger@azcender.com>
2015-05-18 15:09:29 -0400

    Add remove code from proxy config

--- manifests/helper_docker_haproxy_port.pp
+++ manifests/helper_docker_haproxy_port.pp
@@ -3,14 +3,25 @@
 define profile::helper_docker_haproxy_port(
   $ports,
   $listening_service = 'docker',
+  $running = true,
 ) {
   $ports_array = split($name, ':')
 
   $port = $ports_array[0]
 
-  $concat_name = "${name}-${::ipaddress}"
+  $concat_name = "${port}-${::clientcert}"
+
+  # Ensure the container is running. Else we should delete
+  # the rule.
+
+  $ensure = $running ? {
+    true  => present,
+    false => absent,
+  }
+
 
   @@::haproxy::balancermember { $concat_name:
+    ensure            => $ensure,
     listening_service => $listening_service,
     server_names      => $::hostname,
     ipaddresses       => $::ipaddress,

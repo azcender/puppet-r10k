218686728da8d7da57aee912221d7ed1e2c05997
Bryan Belanger <bbelanger@azcender.com>
2015-05-17 21:49:11 -0400

    Test docker profile with iteration

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -10,12 +10,14 @@ class profile::docker {
   $runs = hiera('profile::docker::runs')
   create_resources('::docker::run', $runs)
 
-  $concat_name = "${name}-${::ipaddress_ens33}"
+  $runs.each |$run| {
+    $concat_name = "${name}-${::ipaddress}"
 
-  @@::haproxy::balancermember { $concat_name:
-    listening_service => 'docker',
-    server_names      => $::hostname,
-    ipaddresses       => $::ipaddress_ens33,
-    ports             => '8888',
+    @@::haproxy::balancermember { $concat_name:
+      listening_service => 'docker',
+      server_names      => $::hostname,
+      ipaddresses       => $::ipaddress,
+      ports             => '8888',
+    }
   }
 }

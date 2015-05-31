beb28c1e2bab27a129cc13508ae3931fb1a88b8c
Bryan Belanger <bbelanger@azcender.com>
2015-05-30 22:02:56 -0400

    Try again

--- manifests/tomcat_container.pp
+++ manifests/tomcat_container.pp
@@ -1,31 +1,42 @@
 # A wrapper that contains all thr functionality needed for a standard java web
 # application --- does not support JEE applications
 class profile::tomcat_container {
-  $maven_repo = hiera('profile::tomcat_container::maven_repo')
+  #  $maven_repo = hiera('profile::tomcat_container::maven_repo')
 
-  $groupId    = hiera('profile::tomcat_container::groupId')
-  $artifactId = hiera('profile::tomcat_container::artifactId')
-  $version    = hiera('profile::tomcat_container::version')
-  $packaging  = hiera('profile::tomcat_container::packaging')
+  #$groupId    = hiera('profile::tomcat_container::groupId')
+  #$artifactId = hiera('profile::tomcat_container::artifactId')
+  #$version    = hiera('profile::tomcat_container::version')
+  #$packaging  = hiera('profile::tomcat_container::packaging')
 
-  # Validate Maven coordinates and other strings
-  validate_string($groupId)
-  validate_string($artifactId)
-  validate_string($version)
-  validate_string($maven_repo)
+  ## Validate Maven coordinates and other strings
+  #validate_string($groupId)
+  #validate_string($artifactId)
+  #validate_string($version)
+  #validate_string($maven_repo)
 
-  validate_re($packaging, [
-                'war',
-                'ear',
-                'jar'
-              ])
+  #validate_re($packaging, [
+  #              'war',
+  #              'ear',
+  #              'jar'
+  #            ])
 
-  $_group_id = regsubst($groupId, '\.', '/', 'G')
+  #$_group_id = regsubst($groupId, '\.', '/', 'G')
 
-  $application_url =
-    "${maven_repo}/${_group_id}/${artifactId}/${version}/${artifactId}-${version}.${packaging}"
+  #$application_url =
+  #  "${maven_repo}/${_group_id}/${artifactId}/${version}
+  # /${artifactId}-${version}.${packaging}"
 
-  ::tomcat::war { 'sample.war':
-    war_source    => $application_url,
+  #::tomcat::war { 'sample.war':
+  #  war_source    => $application_url,
+  #}
+
+  # Remove examples
+  file { '/usr/local/tomcat/webapps/examples':
+    ensure => absent,
+  }
+
+  # Remove favicon
+  file { '/usr/local/tomcat/webapps/ROOT/favicon.ico':
+    ensure => absent,
   }
 }

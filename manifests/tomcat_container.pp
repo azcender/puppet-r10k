f87069f5946f3cd46177786bfd6c91419b637ce7
Bryan Belanger <bbelanger@azcender.com>
2015-05-31 20:46:52 -0400

    Move back tomcat_container and future proof

--- manifests/tomcat_container.pp
+++ manifests/tomcat_container.pp
@@ -1,34 +1,33 @@
 # A wrapper that contains all thr functionality needed for a standard java web
 # application --- does not support JEE applications
 node default {
-  #  $maven_repo = hiera('profile::tomcat_container::maven_repo')
+  $maven_repo = hiera('profile::tomcat_container::maven_repo')
 
-  #$groupId    = hiera('profile::tomcat_container::groupId')
-  #$artifactId = hiera('profile::tomcat_container::artifactId')
-  #$version    = hiera('profile::tomcat_container::version')
-  #$packaging  = hiera('profile::tomcat_container::packaging')
+  $group_id    = hiera('profile::tomcat_container::group_id')
+  $artifact_id = hiera('profile::tomcat_container::artifact_id')
+  $version     = hiera('profile::tomcat_container::version')
+  $packaging   = hiera('profile::tomcat_container::packaging')
 
-  ## Validate Maven coordinates and other strings
-  #validate_string($groupId)
-  #validate_string($artifactId)
-  #validate_string($version)
-  #validate_string($maven_repo)
+  # Validate Maven coordinates and other strings
+  validate_string($group_id)
+  validate_string($artifact_id)
+  validate_string($version)
+  validate_string($maven_repo)
 
-  #validate_re($packaging, [
-  #              'war',
-  #              'ear',
-  #              'jar'
-  #            ])
+  validate_re($packaging, [
+                'war',
+                'ear',
+                'jar'
+              ])
 
-  #$_group_id = regsubst($groupId, '\.', '/', 'G')
+  $_group_id = regsubst($group_id, '\.', '/', 'G')
 
-  #$application_url =
-  #  "${maven_repo}/${_group_id}/${artifactId}/${version}
-  # /${artifactId}-${version}.${packaging}"
+  $application_url =
+    "${maven_repo}/${_group_id}/${artifact_id}/${version}/${artifact_id}-${version}.${packaging}"
 
-  #::tomcat::war { 'sample.war':
-  #  war_source    => $application_url,
-  #}
+  ::tomcat::war { 'sample.war':
+    war_source    => $application_url,
+  }
 
   # Remove examples
   file { '/usr/local/tomcat/webapps/examples':

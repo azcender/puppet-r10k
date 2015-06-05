b4bac15acd5022effdd24e578c618d7f7939effc
Bryan Belanger <bbelanger@azcender.com>
2015-06-05 18:26:24 -0400

    Added files setup to oracle build and new oracle krb5.conf file

--- manifests/oracle.pp
+++ manifests/oracle.pp
@@ -1,9 +1,13 @@
 # Sets up the base oracle build
 class profile::oracle (
+  $files,
   $packages,
 ) {
   include ::profile
   
   # Create packages needed for base Oracle build
   package { $packages: }
+
+  # Create files needed for base Oracle build
+  file { $files: }
 }

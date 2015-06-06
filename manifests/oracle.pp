7cd00b3bf8c3e3398137fdeffd8ba22b23702aea
Bryan Belanger <bbelanger@azcender.com>
2015-06-05 20:17:28 -0400

    Add validations and augeas to oracle

--- manifests/oracle.pp
+++ manifests/oracle.pp
@@ -1,5 +1,6 @@
 # Sets up the base oracle build
 class profile::oracle (
+  $augeas,
   $files,
   $groups,
   $packages,
@@ -10,10 +11,16 @@ class profile::oracle (
   # Basic validation of params
   validate_array($packages)
 
+  validate_hash($augeas)
   validate_hash($files)
+  validate_hash($users)
+  validate_hash($groups)
   
   # Create packages needed for base Oracle build
   package { $packages: }
+
+  # Manipulate complex files using augeas
+  create_resources(augeas, $augeas)
 
   # Create files needed for base Oracle build
   create_resources(file, $files)

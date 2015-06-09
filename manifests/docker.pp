5b62231466e9518f2952e36f14cb88a0e4477f78
Bryan Belanger <bbelanger@azcender.com>
2015-06-08 22:13:19 -0400

    Add file lines to docker profile

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -1,16 +1,21 @@
 # Installs the Docker daemon
 #
 
-class profile::docker {
+class profile::docker(
+  $file_lines,
+) {
   # Include base class
   include ::profile
   include ::docker
 
+  # File lines to build
+  create_resources(files_line, $file_lines)
+  
   # Docker runs in docker yaml
   $runs = hiera('profile::docker::runs')
 
-  create_resources('::docker::run', $runs)
+  create_resources(::docker::run, $runs)
 
   # Create haproxy mappings
-  create_resources('::profile::helper_docker_haproxy', $runs)
+  create_resources(::profile::helper_docker_haproxy, $runs)
 }

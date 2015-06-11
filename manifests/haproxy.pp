84ebb7f122f643680a3d4bd2fc135de1b6db3d76
Bryan Belanger <bbelanger@azcender.com>
2015-06-11 14:13:18 -0400

    Accept ipaddress in haproxy profile. Default to ::ipaddress

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -1,12 +1,15 @@
 # Set up a haproxy proxy
 #
 
-class profile::haproxy {
+class profile::haproxy(
+  $ipaddress = $::ipaddress,
+) {
   # Include base class
   include ::profile
   include ::haproxy
 
-  ::Docker::Run <<||>> ->
-    ::Haproxy::Balancermember <<| listening_service == 'docker' |>>
-
+  haproxy::listen { 'docker':
+    ipaddress => $ipaddress,
+    ports     =>  9999,
+  }
 }

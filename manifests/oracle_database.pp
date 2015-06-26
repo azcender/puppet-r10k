1308acc266c832824cf5bba54fa1834ba3468070
Bryan Belanger <bbelanger@azcender.com>
2015-06-26 16:02:41 -0400

    Move concat fragmenbt to oracle_database profile

--- manifests/oracle_database.pp
+++ manifests/oracle_database.pp
@@ -6,4 +6,11 @@ class profile::oracle_database (
   # Pull the instance variables here for use in templates
   $db_instance = hiera('profile::oracle_database::db_instance')
   $grid_instance = hiera('profile::oracle_database::grid_instance')
+
+  # Add concat fragments
+  ::concat::fragment { 'sudoers_oracle_tail' :
+    target  =>'/etc/sudoers.d/oracle',
+    content => template('profile/oracle/oracle_database_sudoers.erb'),
+    order   => '10',
+  }
 }

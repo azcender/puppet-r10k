b469aeb38c9bae7e1857ff5600173f302f28d9ec
Bryan Belanger <bbelanger@azcender.com>
2015-06-26 15:24:56 -0400

    Add oracle db and grid info to manifest

--- manifests/oracle_database.pp
+++ manifests/oracle_database.pp
@@ -2,4 +2,8 @@
 class profile::oracle_database (
 ) {
   include ::profile
+
+  # Pull the instance variables here for use in templates
+  $db_instance = hiera('profile::oracle_database::db_instance')
+  $grid_instance = hiera('profile::oracle_database::grid_instance')
 }

dce81fc04857be669c9b359851fa51a52a6eebfe
Bryan Belanger <bbelanger@azcender.com>
2015-06-27 20:14:21 -0400

    Remove sudoers2

--- manifests/master.pp
+++ manifests/master.pp
@@ -2,12 +2,6 @@
 #
 
 class profile::master {
-  # Set global file settings
-  #file {'/etc/puppetlabs/puppet/modules':
-  #  mode    => 'a+r',
-  #  recurse => true
-  #}
-
   file { '/etc/puppetlabs/puppet/hiera.yaml':
     ensure => 'file',
     owner  => 'root',
@@ -16,20 +10,4 @@ class profile::master {
     source => 'puppet:///modules/profile/hiera.yaml',
     #notify => Service['pe-puppetserver'],
   }
-
-  file { '/opt/puppet/share/augeas/lenses/dist/sudoers2.aug':
-    ensure => 'file',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/profile/oracle/sudoers2.aug',
-    #notify => Service['pe-puppetserver'],
-  }
-
-  #  ini_setting { 'modulepath':
-  #  ensure  => absent,
-  #  path    => '/etc/puppetlabs/puppet/puppet.conf',
-  #  section => 'main',
-  #  setting => 'modulepath',
-  #}
 }

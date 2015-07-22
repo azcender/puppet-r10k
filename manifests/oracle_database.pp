f9bad0c9ea57afc8ff1dae7cba64dec95ab341c2
Ron Compos <rcompos@gmail.com>
2015-07-22 15:43:00 -0600

    EoD push

--- manifests/oracle_database.pp
+++ manifests/oracle_database.pp
@@ -2,11 +2,6 @@
 class profile::oracle_database (
 ) {
   include ::profile
-
-  # Pull the instance variables here for use in templates
-  $db_instance = hiera('profile::oracle_database::db_instance')
-  $grid_instance = hiera('profile::oracle_database::grid_instance')
-
   # Add concat fragments
   #  ::concat::fragment { 'sudoers_oracle_tail' :
   #  target  =>'/etc/sudoers.d/oracle',

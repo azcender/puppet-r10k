f8fd43d1c87a8a396caab293976eac079346fe13
Paul Talbot <paul@fullfrontalingenuity.com>
2015-07-23 18:15:17 -0400

    EoD

--- manifests/oracle_database.pp
+++ manifests/oracle_database.pp
@@ -2,6 +2,8 @@
 class profile::oracle_database (
 ) {
   include ::profile
+  $rac_instance = hiera('profile::oracle_database::rac_instance')
+  Host <<| tag == "${rac_instance}" |>>
   # Add concat fragments
   #  ::concat::fragment { 'sudoers_oracle_tail' :
   #  target  =>'/etc/sudoers.d/oracle',

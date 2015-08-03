23d4ce585b545042b58bf5ca3ac70a72547b1782
Bryan Belanger <bbelanger@azcender.com>
2015-08-03 19:59:16 -0400

    Add adf artifactory resource to tomcat_adf

--- manifests/tomcat_adf.pp
+++ manifests/tomcat_adf.pp
@@ -1,14 +1,12 @@
 # A wrapper that contains all thr functionality needed for a standard java web
 # application --- does not support JEE applications
+#
+#
 class profile::tomcat_adf (
-  $adf_libraries_source
+  $adf_artifactory_location
 ) {
   include ::profile
   include ::profile::tomcat
 
-
-  staging::deploy { 'sample.tar.gz':
-    source => $adf_libraries_source,
-    target => '/opt/tomcat/lib',
-  }
+  create_resources('::archive::artifactory', $adf_artifactory_location)
 }

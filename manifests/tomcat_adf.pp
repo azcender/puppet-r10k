0640b0672ad66c29532c6c88e4cf9cf2c0ad3613
Bryan Belanger <bbelanger@azcender.com>
2015-08-04 22:38:43 -0400

    Finish tomcat adf v1

--- manifests/tomcat_adf.pp
+++ manifests/tomcat_adf.pp
@@ -1,12 +1,20 @@
-# A wrapper that contains all thr functionality needed for a standard java web
-# application --- does not support JEE applications
-#
+# Installs a tomcat with the needed ADF files
 #
 class profile::tomcat_adf (
-  $adf_artifactory_location
+  $adf_files
 ) {
   include ::profile
   include ::profile::tomcat
 
-  create_resources('::archive::artifactory', $adf_artifactory_location)
+  # Ensure adf_files is a hash
+  validate_hash($adf_files)
+
+  # Set defaults for the artifactory resources
+  $adf_files_defaults = {
+    artifactory_host => hiera('artifactory_host'),
+    require          => ::Tomcat::Instance['profile::tomcat'],
+  }
+
+  create_resources('::tomcat::artifactory', $adf_files, $adf_files_defaults)
 }
+

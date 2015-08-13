d3e449ad2f9e1c0183cc8ac5a0cd6149de4c00ea
Bryan Belanger <bbelanger@azcender.com>
2015-08-13 13:45:58 -0400

    Turn catalina.sh change to notify

--- manifests/tomcat_adf.pp
+++ manifests/tomcat_adf.pp
@@ -12,7 +12,7 @@ class profile::tomcat_adf (
   # Set Oracle cache
   file_line { 'adf_catalina_opts':
     require  => ::Tomcat::Instance['profile::tomcat'],
-    before   => ::Tomcat::Service['profile::tomcat'],
+    notify   => ::Tomcat::Service['profile::tomcat'],
     path     => '/opt/tomcat/bin/catalina.sh',
     line     => 'CATALINA_OPTS=-Doracle.mds.cache=simple',
     match    => '^\#   CATALINA_OUT.*',

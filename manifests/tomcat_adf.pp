f7e300455b90871ddd81270b536f16ba2b972ab0
Bryan Belanger <bbelanger@azcender.com>
2015-08-13 13:41:28 -0400

    FIx adf type 2

--- manifests/tomcat_adf.pp
+++ manifests/tomcat_adf.pp
@@ -11,7 +11,7 @@ class profile::tomcat_adf (
 
   # Set Oracle cache
   file_line { 'adf_catalina_opts':
-    require  => ::Tomcat::Instance['profile:tomcat'],
+    require  => ::Tomcat::Instance['profile::tomcat'],
     before   => ::Tomcat::Service['profile::tomcat'],
     path     => '/opt/tomcat/bin/catalina.sh',
     line     => 'CATALINA_OPTS=-Doracle.mds.cache=simple',
